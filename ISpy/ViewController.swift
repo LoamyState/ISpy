8701611c9360203f8fe45c95bfb5ed74177417f2
Jane Madsen <janemadsen815@gmail.com>
2023-03-10 09:36:26 -0700

    Complete ISpy lab with scroll views

--- ISpy/ViewController.swift
+++ ISpy/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet var scrollView: UIScrollView!
+    
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateZoomFor(size: view.bounds.size)
+    }
 
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        let width = size.width / imageView.bounds.width
+        let height = size.height / imageView.bounds.height
+        let scale = min(width, height)
+        scrollView.minimumZoomScale = scale
+        scrollView.zoomScale = scale
+    }
 
 }
 
